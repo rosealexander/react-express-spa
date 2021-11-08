8e096336c8431dd8aa7c47c9eca2a2f03801ec30
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-08 00:53:56 -0800

    Update author.entity.ts

--- src/entity/author.entity.ts
+++ src/entity/author.entity.ts
@@ -1,8 +1,14 @@
-import {Entity, PrimaryGeneratedColumn, Column, OneToMany} from "typeorm";
+import {Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn, UpdateDateColumn} from "typeorm";
 import {Poem} from "./poem.entity";
 
 @Entity()
 export class Author {
+
+    @CreateDateColumn()
+    created_at: Date;
+
+    @UpdateDateColumn()
+    updated_at: Date;
 
     @PrimaryGeneratedColumn()
     id: number;
