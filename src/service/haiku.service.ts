b070dbf105630c5b0952a070076e4351577af83e
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-08 00:54:20 -0800

    Update haiku.service.ts

--- src/service/haiku.service.ts
+++ src/service/haiku.service.ts
@@ -1,18 +1,38 @@
 // filename: haiku.service.ts
-import {Service} from 'typedi';
+import {Inject, Service} from 'typedi';
 import {InjectRepository} from 'typeorm-typedi-extensions';
 import {Haiku} from '../entity/haiku.entity';
 import {Repository} from "typeorm";
-import {PoetryService} from "./poetry.service";
+import {PoemService} from "./poem.service";
+import {HaikuFactory} from "../factories/haiku.factory";
+
 
 @Service()
-export class HaikuService extends PoetryService {
+export class HaikuService extends PoemService {
+
+    public find(id: number): Promise<Haiku> {
+        return this.repository.findOne(id);
+    }
 
     public findAll(): Promise<Haiku[]> {
         return this.repository.find({ relations: ["author"] });
     }
 
-    constructor(@InjectRepository(Haiku) private repository: Repository<Haiku>) {
-        super();
+    public async removeAll(): Promise<Haiku[]> {
+        const list = await this.repository.find()
+        return this.repository.remove(list);
     }
+
+    public generate(): Promise<Haiku> {
+        return this.haikuFactory.run()
+    }
+
+    public save(haiku: Haiku): Promise<Haiku> {
+        return this.repository.save(haiku)
+    }
+
+    constructor(
+        @InjectRepository(Haiku) private repository: Repository<Haiku>,
+        @Inject() private haikuFactory: HaikuFactory
+    ){super()}
 }
