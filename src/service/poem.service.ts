96656e17fa29438c1e15846d720ee055f0aa36bb
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-08 00:54:24 -0800

    Update poem.service.ts

--- src/service/poem.service.ts
+++ src/service/poem.service.ts
@@ -1,23 +1,7 @@
 // filename: poem.service.ts
-import {Inject, Service} from 'typedi';
-import {Limerick} from '../entity/limerick.entity';
-import {Poem} from "../entity/poem.entity";
-import {LimerickService} from "./limerick.service";
-import {HaikuService} from "./haiku.service";
-import {Haiku} from "../entity/haiku.entity";
-import {PoetryService} from "./poetry.service";
-
-
-@Service()
-export class PoemService extends PoetryService{
-
-    public async findAll(): Promise<Poem[]> {
-        const haikus: Haiku[] = await this.haikuService.findAll()
-        const limericks: Limerick[] = await this.limerickService.findAll()
-        return [...haikus, ...limericks];
-    }
-
-    constructor(@Inject() private haikuService: HaikuService, @Inject() private limerickService: LimerickService) {
-        super();
-    }
+export abstract class PoemService {
+    public abstract find(id: number): Promise<any>
+    public abstract findAll(): Promise<any[]>
+    public abstract removeAll(): Promise<any[]>
+    public abstract generate(): Promise<any>
 }
