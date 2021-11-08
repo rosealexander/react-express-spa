c75bdc12a895614439d243c7e308c690f6138348
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-08 00:54:18 -0800

    Create author.service.ts

---
+++ src/service/author.service.ts
@@ -0,0 +1,34 @@
+// filename: author.service.ts
+import {InjectRepository} from 'typeorm-typedi-extensions';
+import {Inject, Service} from 'typedi';
+import {Author} from '../entity/author.entity';
+import {PoemService} from "./poem.service";
+import {Repository} from "typeorm";
+import {AuthorFactory} from "../factories/author.factory";
+
+
+@Service()
+export class AuthorService extends PoemService {
+
+    public find(id: number): Promise<Author> {
+        return this.repository.findOne(id);
+    }
+
+    public findAll(): Promise<Author[]> {
+        return this.repository.find();
+    }
+
+    public async removeAll(): Promise<Author[]> {
+        const list = await this.repository.find()
+        return this.repository.remove(list);
+    }
+
+    public generate(): Promise<Author> {
+        return this.authorFactory.run()
+    }
+
+    constructor(
+        @InjectRepository(Author) private repository: Repository<Author>,
+        @Inject() private authorFactory: AuthorFactory
+    ) {super()}
+}
