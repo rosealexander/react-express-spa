e8676156f8239bed4162153bf276663a4505682f
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-08 00:54:11 -0800

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,10 @@
 import { Router, Request, Response } from "express";
-import poem from "./poem";
+import poetry from "./poetry";
+import author from "./author";
 
 const indexRouter = Router();
 
-indexRouter.use("/poem", poem);
+indexRouter.use("/poetry", poetry);
+indexRouter.use("/author", author);
 
 export default indexRouter;
