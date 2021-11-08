e954b2d27e46be6ca32c7b93aa03aa14d133e0cd
Alexander Rose <38681180+rosealexander@users.noreply.github.com>
2021-11-08 00:54:22 -0800

    Update limerick.service.ts

--- src/service/limerick.service.ts
+++ src/service/limerick.service.ts
@@ -1,18 +1,40 @@
 // filename: limerick.service.ts
-import {Service} from 'typedi';
+import {Inject, Service} from 'typedi';
 import {InjectRepository} from 'typeorm-typedi-extensions';
-import {Limerick} from '../entity/limerick.entity';
 import {Repository} from "typeorm";
-import {PoetryService} from "./poetry.service";
+import {PoemService} from "./poem.service";
+import {Limerick} from "../entity/limerick.entity";
+import {LimerickFactory} from "../factories/limerick.factory";
+import {Author} from "../entity/author.entity";
+import {Haiku} from "../entity/haiku.entity";
+
 
 @Service()
-export class LimerickService extends PoetryService{
+export class LimerickService extends PoemService{
+
+    public find(id: number): Promise<Limerick> {
+        return this.repository.findOne(id);
+    }
 
     public findAll(): Promise<Limerick[]> {
         return this.repository.find({ relations: ["author"] });
     }
 
-    constructor(@InjectRepository(Limerick) private repository: Repository<Limerick>) {
-        super();
+    public async removeAll(): Promise<Limerick[]> {
+        const list = await this.repository.find()
+        return this.repository.remove(list);
     }
+
+    public generate(): Promise<Limerick> {
+        return this.limerickFactory.run()
+    }
+
+    public save(limerick: Limerick): Promise<Limerick> {
+        return this.repository.save(limerick)
+    }
+
+    constructor(
+        @InjectRepository(Limerick) private repository: Repository<Limerick>,
+        @Inject() private limerickFactory: LimerickFactory
+    ){super()}
 }
